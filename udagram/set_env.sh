d2ded26079adeca0e70bf15be9b5ab9e55b1225a
Yassin-Mahmoud <yassinmahmoud004@gmail.com>
2022-11-18 14:17:30 +0200

    commit

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -8,4 +8,4 @@ export AWS_BUCKET=arn:aws:s3:::udagram-yassin12321
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=yassin12321
-export URL=http://udagram-api-dev.eba-p2m4ar2p.us-east-1.elasticbeanstalk.com
+export URL=http://udagram-yassin12321.s3-website-us-east-1.amazonaws.com
