cb9a2c36bd3b7ca1d3ba21637582037509e2dd76
Yassin-Mahmoud <yassinmahmoud004@gmail.com>
2022-11-18 11:12:30 +0200

    commit

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
 	production: false,
 	appName: "Udagram",
-	apiHost: "http://localhost:8080/api/v0",
+	apiHost: "http://udagram-api-dev.eba-p2m4ar2p.us-east-1.elasticbeanstalk.com",
+	// http://localhost:8080/api/v0/
 };
 
 /*
